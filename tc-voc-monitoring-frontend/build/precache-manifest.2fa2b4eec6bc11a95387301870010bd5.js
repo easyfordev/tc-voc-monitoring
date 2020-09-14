78069d30374f3aa762a7ea11abf499c8dde0af12
easyfordev <easyfordev8@gmail.com>
2020-09-14 20:18:10 +0900

    Add build outpus

---
+++ tc-voc-monitoring-frontend/build/precache-manifest.2fa2b4eec6bc11a95387301870010bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59975a0fb065bc1845118959add5f2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2643b731e05a2810d6b1",
+    "url": "/static/css/main.4e514203.chunk.css"
+  },
+  {
+    "revision": "25342671326532be5a71",
+    "url": "/static/js/2.deb52511.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.deb52511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2643b731e05a2810d6b1",
+    "url": "/static/js/main.f9222888.chunk.js"
+  },
+  {
+    "revision": "c2552cedd1bda2e30bab",
+    "url": "/static/js/runtime-main.2cd86821.js"
+  }
+]);
