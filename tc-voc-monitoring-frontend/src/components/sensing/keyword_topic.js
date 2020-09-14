f10abe94122163b0d27227460bad0e9c8be61072
easyfordev <easyfordev8@gmail.com>
2020-09-14 20:17:02 +0900

    Build current status

--- tc-voc-monitoring-frontend/src/components/sensing/keyword_topic.js
+++ tc-voc-monitoring-frontend/src/components/sensing/keyword_topic.js
@@ -1,83 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 /* 
 TODO 
 
-1. redux로 데이터 받는 구조로 바꾸기
-2. 엑셀 다운로드
+1. redux로 데이터 받는 구조로 바꾸기 V
+2. 엑셀 다운로드 
 3. 특정 유형 클릭하면, 해당 유형 키워드/토픽 나오도록 처리 -> Classification
-
 */
 class KeywordTopic extends Component {
     constructor(props) {
         super(props);
 
         this.state = { 
-            totalKeyword: [
-                {   keyword: '카카오톡', 
-                    voclist: [ {id: '1', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '2', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '3', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   keyword: '초기화', 
-                    voclist: [ {id: '4', dt: '20200910', hhmmss: '102023', memo: '초기화 안내. 고층 특성 안내 어쩌고'},
-                    {id: '5', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '6', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   keyword: '중계기', 
-                    voclist: [ {id: '7', dt: '20200910', hhmmss: '102023', memo: '중계기 고장난듯 불량. 고저쩌고'},
-                    {id: '8', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '9', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   keyword: '이용', 
-                    voclist: [ {id: '10', dt: '20200910', hhmmss: '102023', memo: '이용 실패 쩌고'},
-                    {id: '11', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '12', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   keyword: '동일', 
-                    voclist: [ {id: '13', dt: '20200910', hhmmss: '102023', memo: '동일 동일 저꼬저쩌고저쩌고'},
-                    {id: '14', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '15', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                }
-            ],
             showTotalKeywordDetail: [ true, false, false, false, false ],
-            totalTopic: [
-                {   topic: '카카오톡, 카톡, 오류, 장애, 정상', 
-                    voclist: [ {id: '1', dt: '20200910', hhmmss: '102023', memo: '카카오톡 어쩌고 저쩌고'},
-                    {id: '2', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '3', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   topic: '연락, 추가, 불량, 상호명, 요청', 
-                    voclist: [ {id: '4', dt: '20200910', hhmmss: '102023', memo: '초기화 안내. 고층 특성 안내 어쩌고'},
-                    {id: '5', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '6', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   topic: '위치, 통화, 데이터, 불량, 권유', 
-                    voclist: [ {id: '7', dt: '20200910', hhmmss: '102023', memo: '중계기 고장난듯 불량. 고저쩌고'},
-                    {id: '8', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '9', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   topic: '데이터, 초기화, 정상, 위치, 설정', 
-                    voclist: [ {id: '10', dt: '20200910', hhmmss: '102023', memo: '이용 실패 쩌고'},
-                    {id: '11', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '12', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                },
-                {   topic: '불량, 특이, 품질, 통화, 기타', 
-                    voclist: [ {id: '13', dt: '20200910', hhmmss: '102023', memo: '동일 동일 저꼬저쩌고저쩌고'},
-                    {id: '14', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'},
-                    {id: '15', dt: '20200910', hhmmss: '102023', memo: '자택 내 통화품질 불량. 고층 특성 안내 어저꼬저쩌고저쩌고'}
-                    ] 
-                }
-            ],
             showTotalTopicDetail: [ true, false, false, false, false ]
         }
         this.renderKeywordListItem = this.renderKeywordListItem.bind(this);
@@ -87,13 +23,13 @@ class KeywordTopic extends Component {
 
     // 1. Keyword
     renderKeywordListItem(){
-        const KeywordItems = this.state.totalKeyword.map( (keyword, index) => {
+        const KeywordItems = this.props.totalKeyword.map( (keyword, index) => {
             return (
                 <div>
                     <div className="boxitem" key={keyword.keyword} onClick={this.onClickTotalKeyword.bind(this, index)}>
                         <span id="number">{index +1}위</span>
                         <span id="text">{keyword.keyword}</span>
-                        <button>Excel</button>
+                        <button onClick={this.onClickKeywordExcelButton}>Excel</button>
                     </div> 
                     {this.renderVocItem(keyword.voclist, index)}
                 </div>
@@ -129,7 +65,7 @@ class KeywordTopic extends Component {
 
     // 2. Topic(Key Sentence)
     renderTopicListItem(){
-        const KeywordItems = this.state.totalTopic.map( (topic, index) => {
+        const KeywordItems = this.props.totalTopic.map( (topic, index) => {
             return (
                 <div>
                     <div className="boxitem" key={topic.topic} onClick={this.onClickTotalTopic.bind(this, index)}>
@@ -168,6 +104,11 @@ class KeywordTopic extends Component {
 
         this.setState({showTotalTopicDetail: temp});
     }
+
+    onClickKeywordExcelButton(){
+        console.log("Clicked!");
+    }
+
     render() {
         return (
             <div className="KeywordTopic">
@@ -198,4 +139,9 @@ class KeywordTopic extends Component {
     }
 }
 
-export default KeywordTopic;
+
+function mapStateToProps(state) { 
+    return { totalKeyword : state.totalKeyword, totalTopic: state.totalTopic };  
+}
+
+export default connect(mapStateToProps) (KeywordTopic);
--- tc-voc-monitoring-frontend/src/reducers/index.js
+++ tc-voc-monitoring-frontend/src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import PeriodReducer from './reducer_period';
 import Class2Reducer from './reducer_class2';
 import Class3Reducer from './reducer_class3';
+import TotalKeywordReducer from './reducer_total_keyword';
+import TotalTopicReducer from './reducer_total_topic';
 
 import { reducer as formReducer } from 'redux-form';
 
@@ -9,6 +11,8 @@ const rootReducer = combineReducers({
     period: PeriodReducer,
     class2: Class2Reducer,
     class3: Class3Reducer,
+    totalKeyword: TotalKeywordReducer,
+    totalTopic: TotalTopicReducer,
     form : formReducer
 });
 
